3de66059d7512c28c56da80591a7436db544ffeb
Duong Lam <dzuongld@gmail.com>
2018-11-18 17:38:02 +1100

    Added authentication
    -user can login/logout
    -navlinks updated
    -redirections after successful login/logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  password: string;
+  email: string;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.auth.getAuth().subscribe(auth => {
+      // check if logged in
+      if (auth) {
+        this.router.navigate(['/']); // redirect to dashboard
+      }
+    });
+  }
+
+  onSubmit() {
+    // .then on returned promised
+    this.auth.login(this.email, this.password)
+      .then(res => {
+        this.flashMessage.show('You are now logged in!', {
+          cssClass: 'alert-success', timeout: 4000
+        });
+        this.router.navigate(['/']); // go to dashboard if success
+      })
+      .catch(err => {
+        this.flashMessage.show(err.message, {
+          cssClass: 'alert-danger', timeout: 4000
+        });
+        // this.router.navigate(['/login']); // go back to login on failure
+      });
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private auth: AngularFireAuth
+  ) { }
+
+  login(email: string, password: string) {
+    // return a new promise
+    return new Promise((resolve, reject) => {
+      this.auth.auth.signInWithEmailAndPassword(email, password)
+        .then(userData => resolve(userData), error => reject(error));
+    });
+  }
+
+  getAuth() {
+    return this.auth.authState.pipe(map(auth => auth));
+  }
+
+  logout() {
+    this.auth.auth.signOut();
+  }
+}
